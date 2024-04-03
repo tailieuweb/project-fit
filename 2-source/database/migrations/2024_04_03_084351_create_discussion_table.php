dc5698fc461b2f4b816939e47778f8a0fe3adb1c
tailieuweb <tailieuweb.com@gmail.com>
2024-04-03 16:41:49 +0700

    test

--- 2-source/database/migrations/2024_04_03_084351_create_discussion_table.php
+++ 2-source/database/migrations/2024_04_03_084351_create_discussion_table.php
@@ -5,12 +5,12 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Foostart\Category\Helpers\FoostartMigration;
 
-class CreateForumTable extends FoostartMigration
+class CreateDiscussionTable extends FoostartMigration
 {
     public function __construct()
     {
-        $this->table = 'forum_questions';
-        $this->prefix_column = 'forum_questions_';
+        $this->table = 'forum_discussions';
+        $this->prefix_column = 'forum_discussions_';
     }
 
     /**
@@ -26,18 +26,14 @@ class CreateForumTable extends FoostartMigration
             $table->increments($this->prefix_column . 'id')->comment('Primary key');
 
             // Relation
-            $table->integer('category_id')->nullable()->comment('Category ID');
+            $table->integer('form_questions_id')->comment('Question ID');
 
             // Other attributes
-            $table->string($this->prefix_column . 'title', 255)->comment('Question title');
-            $table->integer($this->prefix_column . 'order')->nullable()->comment('Order in list of categories');
-            $table->string($this->prefix_column . 'slug', 1000)->comment('Slug in URL');
-            $table->string($this->prefix_column . 'overview', 1000)->nullable()->comment('Forum overview');
-            $table->text($this->prefix_column . 'description')->comment('Forum description');
-            $table->string($this->prefix_column . 'image', 255)->nullable()->comment('Image path');
+            $table->integer($this->prefix_column . 'order')->nullable()->comment('Order in list of answer');
+            $table->text($this->prefix_column . 'description')->comment('Answer for question');
             $table->string($this->prefix_column . 'files', 1000)->nullable()->comment('The list of attachment filenames');
-            $table->integer('views')->unsigned()->default('0')->comment('Number of views');
-            $table->boolean('is_ansswerd')->default(0);
+            $table->integer('likes')->unsigned()->default('0')->comment('Number of views');
+            $table->boolean('is_best_answer')->default(0);
             //Set common columns
             $this->setCommonColumns($table);
         });
